a11d55fb1e7d8707a6f7e5c343e8a53a5f9f5684
mesour <matous.nemec@gmail.com>
2017-01-11 23:28:17 +0100

    Revert "minor changes"
    This reverts commit 71b9074990a4654f68fdd33f81d95b3b75ca3e96.

--- src/Mesour/UI/Editable.php
+++ src/Mesour/UI/Editable.php
@@ -287,6 +287,8 @@ class Editable extends Mesour\Components\Control\AttributesControl
 		try {
 			$currentField = $this->getDataStructure()->getField($name);
 
+
+
 			PermissionsChecker::check(PermissionsChecker::CREATE, $this, $currentField);
 
 			$reference = $currentField->getReference();
