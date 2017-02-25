d3191d79abe521796c0939aef398d5f6f3f5b95b
mesour <matous.nemec@gmail.com>
2017-02-25 15:32:12 +0100

    minor changes

--- src/Mesour/Editable/Structures/Fields/BaseField.php
+++ src/Mesour/Editable/Structures/Fields/BaseField.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the Mesour Editable (http://components.mesour.com/component/editable)
  *
- * Copyright (c) 2016 Matouš Němec (http://mesour.com)
+ * Copyright (c) 2017 Matouš Němec (http://mesour.com)
  *
  * For full licence and copyright please view the file licence.md in root of this project
  */
